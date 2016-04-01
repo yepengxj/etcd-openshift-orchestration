afb85e1b85e5050bb5bd0895aff6c3d4a482e499
HeHongling <hank.sunday@gmail.com>
2016-04-01 08:17:37 +0800

    5

--- etcd-join.sh
+++ etcd-join.sh
@@ -6,8 +6,6 @@ echo "join" $2 "into" $1 "with" $3
 
 etcdctl --endpoints $1 member remove $2
 eval `etcdctl --endpoints $1 member add $2 $3` 
-etcd -name $2 -initial-advertise-peer-urls $3 \
--listen-peer-urls http://0.0.0.0:2380 -listen-client-urls http://0.0.0.0:2379  \ 
--advertise-client-urls $1 \
+etcd -name $2 -initial-advertise-peer-urls $3 -listen-peer-urls http://0.0.0.0:2380 -listen-client-urls http://0.0.0.0:2379 -advertise-client-urls $1 
 
 
