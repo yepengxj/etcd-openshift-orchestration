d8570b7d2d8c0ebe182d394fa65115e150da780f
HeHongling <hank.sunday@gmail.com>
2016-04-01 15:53:16 +0800

    4

--- etcd-join.sh
+++ etcd-join.sh
@@ -9,7 +9,11 @@ echo "----->remove $2"
 `etcdctl member remove $2 2>&1 |grep  "etcdctl member remove"`
 sleep 5
 echo "----->add $2 $3"
-`etcdctl member add $2 $3|grep ETCD |awk '{print "export " $0}'`
+`etcdctl member add $2 $3|grep ETCD_INITIAL_CLUSTER |awk '{print "export " $0}'`
+
+export ETCD_NAME=$2
+export ETCD_INITIAL_CLUSTER_STATE=existing
+
 sleep 5
 echo "----->start etcd"
 etcd -name $2 -initial-advertise-peer-urls $3 -listen-peer-urls http://0.0.0.0:2380 -listen-client-urls http://0.0.0.0:2379 -advertise-client-urls $1 
