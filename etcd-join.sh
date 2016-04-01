0d1b9cbfbf4cd9d30c776c1e56dde7616d7996ab
HeHongling <hank.sunday@gmail.com>
2016-04-01 18:45:14 +0800

    222

--- etcd-join.sh
+++ etcd-join.sh
@@ -1,5 +1,6 @@
 # $1 $2 $3 $4
 # endpoints newnode initial-advertise-peer-urls sb-instanceid-etcd
+set -x 
 
 echo "----->join" $2 "into" $1 "with" $3  "use id " $4
 
@@ -12,7 +13,7 @@ tmpnode=`etcdctl member list |grep $3|awk -F[ '{print $1}' `
 etcdctl member remove $tmpnode
 
 echo "----->add $2 $3"
-`etcdctl member add $2 $3|grep ETCD_INITIAL_CLUSTER |awk '{print "export " $0}'`
+`etcdctl member add $2 $3 | grep ETCD_INITIAL_CLUSTER |awk '{print "export " $0}'`
 export ETCD_INITIAL_CLUSTER_STATE=existing
 export ETCD_NAME=$2
 
