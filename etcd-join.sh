e3379f8122ede9edd93d22b60a76d9dc082612a1
HeHongling <hank.sunday@gmail.com>
2016-04-01 15:17:23 +0800

    test

--- etcd-join.sh
+++ etcd-join.sh
@@ -1,12 +1,17 @@
 # $1 $2 $3 
 # endpoints newnode initial-advertise-peer-urls 
 
-echo "join" $2 "into" $1 "with" $3
+echo "----->join" $2 "into" $1 "with" $3
 
 export ETCDCTL_ENDPOINT=$1
 
+echo "----->remove $2"
 `etcdctl member remove $2 2>&1 |grep  "etcdctl member remove"`
+sleep 5
+echo "----->add $2"
 `etcdctl member add $2 $3|grep ETCD |awk '{print "export " $0}'`
+sleep 5
+echo "----->start etcd"
 etcd -name $2 -initial-advertise-peer-urls $3 -listen-peer-urls http://0.0.0.0:2380 -listen-client-urls http://0.0.0.0:2379 -advertise-client-urls $1 
 
 
