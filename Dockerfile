20c8a116fa8bc3cd19998089302ce53787ff0425
zonesan <san@zoobook.org>
2016-04-18 14:57:43 +0800

    update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ FROM alpine
 #RUN curl -L  https://github.com/coreos/etcd/releases/download/v2.3.0/etcd-v2.3.0-linux-amd64.tar.gz -o /etcd-v2.3.0-linux-amd64.tar.gz && tar xzvf /etcd-v2.3.0-linux-amd64.tar.gz
 ADD etcd-v2.3.0-linux-amd64.tar.gz /usr/bin
 #ADD . /usr/bin
+ADD bash /usr/bin
---
+++ bash/etcd-join.sh
@@ -0,0 +1,23 @@
+# $1 endpoints
+# $2 newnode 
+# $3 initial-advertise-peer-urls 
+# $4 sb-instanceid-etcd
+# $5 rootpasswd
+
+echo "----->join" $2 "into" $1 "with" $3  "use id " $4
+
+export ETCDCTL_ENDPOINT=$1
+
+echo "----->remove $3"
+tmpnode=`etcdctl -u root:$5 member list |grep $3|awk -F: '{print $1}' `
+etcdctl -u root:$5 member remove $tmpnode
+
+echo "----->add $2 $3"
+eval `etcdctl -u root:$5 member add $2 $3 | grep ETCD_INITIAL_CLUSTER`
+export ETCD_INITIAL_CLUSTER_STATE=existing
+export ETCD_NAME=$2
+
+echo "----->start etcd"
+etcd -initial-cluster $ETCD_INITIAL_CLUSTER -initial-cluster-token $4 -initial-advertise-peer-urls $3 -listen-peer-urls http://0.0.0.0:2380 -listen-client-urls http://0.0.0.0:2379 -advertise-client-urls $1 
+
+
