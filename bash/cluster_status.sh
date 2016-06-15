23642e2b5a7aadf3793d72f21381047d2d15ee0d
yepeng <yepeng>
2016-06-15 19:00:58 +0800

    modified:   cluster_status.sh

--- bash/cluster_status.sh
+++ bash/cluster_status.sh
@@ -1,6 +1,6 @@
-echo "detect etcd cluster status"
+#echo "detect etcd cluster status"
 cluster_status=$(etcdctl -u root:$1 cluster-health | tail -n 1 | awk '{print $3}')
-echo "etcd cluster stuats: "$cluster_status
+#echo "etcd cluster stuats: "$cluster_status
 
 if [ "$cluster_status"x = "unhealthy"x ]; then
 exit 1
