b1ae55915237014065459a2015a2e30d9f965e87
yepeng <yepeng>
2016-06-15 17:16:22 +0800

    new file:   bash/cluster_status.sh  	new file:   test-etcd0.yaml

---
+++ bash/cluster_status.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "detect etcd cluster status"
+cluster_status=$(etcdctl -u root:$1 cluster-health | tail -n 1 | awk '{print $3}')
+echo "etcd cluster stuats: "$cluster_status
+
+if [ "$cluster_status"x = "healthy"x ]; then
+exit 0
+else
+exit 1
+fi
