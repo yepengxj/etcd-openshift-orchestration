bcf041454479ddeed005f3aa72f40869296cb65a
yepengxj <yepeng_xj@163.com>
2016-06-17 13:25:53 +0800

    modified:   bash/etcd-init.sh

--- bash/etcd-init.sh
+++ bash/etcd-init.sh
@@ -10,61 +10,60 @@
 # $5
 
 member_count=$(etcdctl -u root:$1 member list |wc -l)
-if [ $member_count == 0  ]; then
-         echo 0
-        etcd -name \
-         etcd0 \
-         -initial-advertise-peer-urls \
-         $2 \
-         -listen-peer-urls \
-         http://0.0.0.0:2380 \
-         -listen-client-urls \
-         http://0.0.0.0:2379 \
-         -advertise-client-urls \
-         $3 \
-         -initial-cluster-token \
-         $4 \
-         -initial-cluster \
-         etcd0=$2 \
-         -initial-cluster-state \
-         new
+echo member_count: $member_count
+if [ $member_count == 0 ]; then
+     echo 0
+    etcd -name \
+     etcd0 \
+     -initial-advertise-peer-urls \
+     $2 \
+     -listen-peer-urls \
+     http://0.0.0.0:2380 \
+     -listen-client-urls \
+     http://0.0.0.0:2379 \
+     -advertise-client-urls \
+     $3 \
+     -initial-cluster-token \
+     $4 \
+     -initial-cluster \
+     etcd0=$2 \
+     -initial-cluster-state \
+     new
+    etcdctl user add root << EOF
+    $1
+EOF
 
-        etcdctl user add root << EOF
-        $1
-        EOF
+    etcdctl auth enable
 
-        etcdctl auth enable
+    etcdctl -u root:$1 role revoke guest --path '/*' -readwrite
 
-        etcdctl -u root:$1 role revoke guest --path '/*' -readwrite
+    etcdctl -u root:$1 role add binduser
 
-        etcdctl -u root:$1 role add binduser
+    etcdctl -u root:$1 role grant binduser -path '/*' -readwrite
 
-        etcdctl -u root:$1 role grant binduser -path '/*' -readwrite
+    etcdctl -u root:$3 user add $1 << EOF
+    $5
+EOF
 
-        etcdctl -u root:$3 user add $1 << EOF
-        $5
-        EOF
+    etcdctl -u root:$3 user grant $1 -roles binduser
 
-        etcdctl -u root:$3  user grant $1 -roles binduser
-
-        etcdctl -u root:$3 user passwd $1 << EOF
-        $5
-        EOF
-
+    etcdctl -u root:$3 user passwd $1 << EOF
+    $5
+EOF
 else
-        echo 1
-        export ETCDCTL_ENDPOINT=$3
+    echo 1
+    export ETCDCTL_ENDPOINT=$3
 
-        echo "----->remove $2"
-        tmpnode=`etcdctl -u root:$5 member list |grep $2|awk -F: '{print $1}' `
-        etcdctl -u root:$1 member remove $tmpnode
+    echo "----->remove $2"
+    tmpnode=`etcdctl -u root:$5 member list |grep $2|awk -F: '{print $1}' `
+    etcdctl -u root:$1 member remove $tmpnode
 
-        echo "----->add $2 $3"
-        eval `etcdctl -u root:$5 member add etcd0 $2 | grep ETCD_INITIAL_CLUSTER`
-        export ETCD_INITIAL_CLUSTER_STATE=existing
-        export ETCD_NAME=etcd0
+    echo "----->add $2 $3"
+    eval `etcdctl -u root:$5 member add etcd0 $2 | grep ETCD_INITIAL_CLUSTER`
+    export ETCD_INITIAL_CLUSTER_STATE=existing
+    export ETCD_NAME=etcd0
 
-        echo "----->start etcd"
-        etcd -initial-cluster $ETCD_INITIAL_CLUSTER -initial-cluster-token $4 -initial-advertise-peer-urls $2 -listen-peer-urls http://0.0.0.0:2380 -listen-client-urls http://0.0.0.0:2379 -advertise-client-urls $3
+    echo "----->start etcd"
+    etcd -initial-cluster $ETCD_INITIAL_CLUSTER -initial-cluster-token $4 -initial-advertise-peer-urls $2 -listen-peer-urls http://0.0.0.0:2380 -listen-client-urls http://0.0.0.0:2379 -advertise-client-urls $3
 fi
 
